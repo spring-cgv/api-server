0ef4363159ffa6a58230fbe74482d75824309d63
gh1224 <rlarkgus0@naver.com>
2022-06-13 00:42:28 +0900

    fix: @Query 사용 시 @Param 명시

--- src/main/java/com/cgv/repository/ScheduleRepository.java
+++ src/main/java/com/cgv/repository/ScheduleRepository.java
@@ -3,14 +3,15 @@ package com.cgv.repository;
 import com.cgv.domain.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     @Query("SELECT s FROM Schedule s WHERE FORMATDATETIME(s.startDateTime, 'yyyy-MM-dd') = :screenDate")
-    List<Schedule> findByScreenDate(LocalDate screenDate);
+    List<Schedule> findByScreenDate(@Param("screenDate") LocalDate screenDate);
 
     @Query("SELECT s FROM Schedule s WHERE s.movie.id = :movieId AND FORMATDATETIME(s.startDateTime, 'yyyy-MM-dd') = :screenDate")
-    List<Schedule> findByMovieIdAndScreenDate(Long movieId, LocalDate screenDate);
+    List<Schedule> findByMovieIdAndScreenDate(@Param("movieId") Long movieId, @Param("screenDate") LocalDate screenDate);
 }
--- src/main/java/com/cgv/repository/TicketRepository.java
+++ src/main/java/com/cgv/repository/TicketRepository.java
@@ -4,12 +4,13 @@ import com.cgv.domain.entity.Ticket;
 import com.cgv.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     @Query("SELECT t.user FROM Ticket t JOIN t.schedule s WHERE s.movie.id = :movieId")
-    List<User> findTicketedUsersByMovieId(Long movieId);
+    List<User> findTicketedUsersByMovieId(@Param("movieId") Long movieId);
 
     List<Ticket> findByUserUsername(String username);
 }
