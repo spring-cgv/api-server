f1f78d7d97aa18e035e9a1831e3ff215945a5052
gh1224 <rlarkgus0@naver.com>
2022-06-12 20:09:47 +0900

    feat: 요청 로깅 필터 등록

--- src/main/java/com/cgv/web/config/AppConfig.java
+++ src/main/java/com/cgv/web/config/AppConfig.java
@@ -1,9 +1,22 @@
 package com.cgv.web.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @EnableJpaAuditing
 @Configuration
 public class AppConfig {
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludeHeaders(false);
+        filter.setIncludePayload(true);
+        filter.setIncludeClientInfo(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setAfterMessagePrefix("REQUEST DATA : ");
+        return filter;
+    }
 }
